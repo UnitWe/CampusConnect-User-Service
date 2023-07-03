4e2e80270d4ea9db3e9cf22ed722e0797aa2556d
GabLunaDev <gabriellunacv@gmail.com>
2023-07-03 06:46:16 -0300

    Starting DB connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,28 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { UserModule } from './modules/user/user.module';
 import { PublicationModule } from './modules/publication/publication.module';
 import { AuthModule } from './modules/auth/auth.module';
+import { SequelizeModule } from '@nestjs/sequelize';
 import { LoggerModule } from './modules/logger/logger.module';
+import { DatabaseModule } from './core/database/database.module';
 
 @Module({
-  imports: [UserModule, PublicationModule, AuthModule, LoggerModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true }),
+  UserModule, 
+  PublicationModule, 
+  AuthModule, 
+  LoggerModule,
+  SequelizeModule.forRoot({
+    dialect: 'postgres',
+    host: process.env.DB_HOST,
+    port: 5432,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    models: [],
+  }), DatabaseModule,],
   controllers: [],
   providers: [],
 })
-export class AppModule {}
+export class AppModule { }
---
+++ src/utils/common.ts
@@ -0,0 +1,10 @@
+import * as bcrypt from "bcryptjs";
+
+export async function hashPassword(password: string): Promise<string> {
+    const saltRounds = 10;
+    const salt = await bcrypt.genSalt(saltRounds);
+    const hashedPassword = await bcrypt.hash(password, salt);
+    return hashedPassword;
+}
+  
+
