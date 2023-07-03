93f9afee91fcc83796f77e43fd28c5446ddc9935
GabLunaDev <gabriellunacv@gmail.com>
2023-07-03 15:08:05 -0300

    Configurating application with new features

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
+import * as dotenv from 'dotenv';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+
+dotenv.config();
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.useBodyParser("json")
+  app.setGlobalPrefix("api/v1");
+  await app.listen(process.env.PORT || 5000);
 }
 bootstrap();
